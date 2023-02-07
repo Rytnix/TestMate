e40405acf94aa6c6edaaf7a4f79c993a7815eac2
Utkarsh-Proeffico <utkarsh@proeffico.com>
2023-02-08 00:23:47 +0530

    Initial Commt

---
+++ src/main/java/com/collegeproject/Testmate/TestmateApplication.java
@@ -0,0 +1,13 @@
+package com.collegeproject.Testmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collegeproject/Testmate/TestmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegeproject.Testmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
